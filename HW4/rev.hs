c8ad27f02944f97722f044efcffbddc7aaab38a1
Golf2546 <Pattarapolnanakorn2546@gmail.com>
2024-11-23 16:00:41 +0700

    Modify comment

--- HW4/rev.hs
+++ HW4/rev.hs
@@ -10,7 +10,7 @@ rev ([]) = []
 rev (x:xs) = join(rev(xs),[x]) 
 
 -- How long does join take?
--- O(n) ; When n is number of elements
+-- O(n) ; When n is number of all elements in list 1 and list 2
 
 -- How long does rev take to compute the reverse of a list?
 -- O(n^2) ; When n is number of elements because join use O(n) and rev use join it will be O(n^2)
