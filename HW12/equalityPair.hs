42d372df47730174ce1ace06d94018f018d4b276
GOLF2546 <104701283+GOLF2546@users.noreply.github.com>
2024-12-31 09:21:25 +0700

    Fix comment typo in isEqual function for pair equality check

--- HW12/equalityPair.hs
+++ HW12/equalityPair.hs
@@ -6,4 +6,4 @@
 isEqual :: (Eq a, Eq b) => (a,b) -> (a,b) -> Bool
 isEqual (x1, y1) (x2, y2) = x1 == x2 && y1 == y2
 
--- Both of element is both pair should be equal
+-- Both element in both pair should be equal
