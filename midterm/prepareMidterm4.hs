475d31c9456e0a0117edcb2b5266776a3a680fbf
GOLF2546 <104701283+GOLF2546@users.noreply.github.com>
2025-01-02 09:59:51 +0700

    Add tree traversal and n-ary tree functions, including height and sorting utilities

--- midterm/prepareMidterm4.hs
+++ midterm/prepareMidterm4.hs
@@ -24,3 +24,61 @@ treefoldl f acc (Node l v r) =
 
 tree2 :: Tree Integer
 tree2 = Node (Node (Node (Node Empty 1 Empty) 2 (Node Empty 3 Empty)) 4 (Node Empty 5 Empty)) 6 (Node (Node (Node Empty 7 (Node (Node Empty 8 Empty) 9 (Node Empty 10 Empty))) 11 (Node Empty 12 Empty)) 13 (Node (Node Empty 14 Empty) 15 (Node Empty 16 Empty)))
+
+treefold :: (a -> b -> b -> b) -> b -> Tree a -> b
+treefold _ acc Empty = acc
+treefold f acc (Node l v r) =
+    let acc' = treefold f acc l
+        acc'' = treefold f acc r
+    in f v acc' acc''
+
+simpleTree :: Tree Integer
+simpleTree = Node (Node Empty 1 Empty) 2 (Node Empty 3 Empty)
+
+emptyTree :: Tree Integer
+emptyTree = Empty
+
+-- tree traversal with treefold
+
+preorder' :: Tree a -> [a]
+preorder' Empty = []
+preorder' tree = treefold (\v al ar -> v : al ++ ar) [] tree
+
+inorder' :: Tree a -> [a]
+inorder' Empty = []
+inorder' tree = treefold (\v al ar -> al ++ [v] ++ ar) [] tree
+
+postorder' :: Tree a -> [a]
+postorder' Empty = []
+postorder' tree = treefold (\v al ar -> al ++ ar ++ [v]) [] tree
+
+-- n-ary tree
+data NAryTree a = EmptyTree | NAryNode a [NAryTree a]
+
+nAryPreorder :: NAryTree a -> [a]
+nAryPreorder EmptyTree = []
+nAryPreorder (NAryNode v children) = [v] ++ concat (map nAryPreorder children)
+
+nAryPostorder :: NAryTree a -> [a]
+nAryPostorder EmptyTree = []
+nAryPostorder (NAryNode v children) = concat (map nAryPostorder children) ++ [v]
+
+nAryFold :: (a -> [b] -> b) -> b -> (NAryTree a) -> b
+nAryFold _ acc EmptyTree = acc
+nAryFold f acc (NAryNode v children) = f v $ map (nAryFold f acc) children
+
+height :: Integral p => Tree a -> p
+height Empty = 0
+height (Node l _ r) = 1 + max (height l) (height r)
+
+inorder :: Tree a -> [a]
+inorder Empty = []
+inorder (Node l x r) = inorder l ++ [x] ++ inorder r
+
+qsort :: (Eq a, Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort [lt | lt <- xs , lt < x] ++ [x] ++ qsort [gt | gt <- xs, gt >= x]
+
+isBST :: Ord a => Tree a -> Bool
+isBST Empty = True
+isBST tree = inorder tree == qsort (inorder tree)
