7ed38acad3aece10787b51be3f867e8b4b278efb
GOLF2546 <104701283+GOLF2546@users.noreply.github.com>
2025-02-09 13:41:29 +0700

    Add number guessing game implementation with input validation and range checking

---
+++ HW20/guessing_Number.hs
@@ -0,0 +1,61 @@
+import Text.Read
+import System.Random
+
+verdict :: Int -> Int -> Either String String
+verdict target guess = do
+    case compare guess target of
+        EQ -> Right "You win!"
+        LT -> Left "Too low"
+        GT -> Left "Too high"
+
+readTarget :: IO Int
+readTarget = do
+    readNumber "Target number"
+
+readNumber :: String -> IO Int
+readNumber msg = do
+    putStr $ msg ++ ": "
+    line <- getLine
+    case readEither line :: Either String Int of
+        Left e -> do
+            putStrLn e
+            readNumber msg
+        Right n -> return n
+
+rangeForGuess :: Int -> Int -> (Int, Int)
+rangeForGuess minVal maxVal = (minVal, maxVal)
+
+isInRange :: Int -> (Int, Int) -> Bool
+isInRange guess (minVal, maxVal) = guess > minVal && guess < maxVal
+
+runGame :: Int -> Int -> Int -> (Int -> Bool) -> (Int, Int) -> IO ()
+runGame num lim count cont range = do
+    putStrLn $ "Guess in this range " ++ show (fst range) ++ " to " ++ show (snd range) ++ " and now you have " ++ show (lim - count) ++ " chance."
+    guess <- readNumber "Guess"
+    if not (isInRange guess range)
+        then do
+            putStrLn $ "Not in range! Please guess between " ++ show (fst range) ++ " and " ++ show (snd range)
+            runGame num lim count cont range
+        else do
+            let v = verdict num guess
+            case v of
+                Right m -> do
+                    putStrLn m
+                Left m -> do
+                    putStrLn m
+                    let newRange = case m of
+                            "Too high" -> rangeForGuess (fst range) guess
+                            "Too low" -> rangeForGuess guess (snd range)
+                            _ -> range
+                    if cont count
+                        then runGame num lim (count+1) cont newRange
+                        else putStrLn "Game over"
+
+randomGame :: IO ()
+randomGame = do
+    gen <- newStdGen
+    lim <- readNumber "Guess limit"
+    let target = fst $ uniform gen
+        initialRange = rangeForGuess minBound maxBound
+    putStrLn $ "I've picked a number. Try to guess it! " ++ show target
+    runGame target lim 0 (<(lim-1)) initialRange
